f198968395d45fb67232e1dd6733868799d5ca0f
edehemma09 <edehemmanuelebubechukwu@gmail.com>
2022-06-09 08:06:45 +0100

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DepartmentModule } from './department/department.module';
 import { CourseModule } from './course/course.module';
+import { FacultyModule } from './faculty/faculty.module';
 
 @Module({
-  imports: [DepartmentModule, CourseModule],
+  imports: [DepartmentModule, CourseModule, FacultyModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
