17724493730d46655961a3e390adfcffa2095ccc
EbereJoan <touch .gitignorewittybere@gmail.com>
2022-06-10 10:03:39 +0100

    Email

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DepartmentModule } from './department/department.module';
 import { CourseModule } from './course/course.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [DepartmentModule, CourseModule],
+  imports: [DepartmentModule, CourseModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
